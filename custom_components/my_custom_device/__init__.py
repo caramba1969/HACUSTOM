ae67f68a303872e50ec47f89d123818ce57f0e75
Remco <remco@vanbeek.casa>
2026-01-01 20:26:54 +0100

    Refactor My Custom Device integration setup and clean up unused code

--- custom_components/my_custom_device/__init__.py
+++ custom_components/my_custom_device/__init__.py
@@ -1,42 +1,13 @@
 """The My Custom Device integration."""
-from __future__ import annotations
-
 import logging
-from typing import Any
 
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import ConfigEntryNotReady
-
-from .const import DOMAIN
+from homeassistant.helpers.typing import ConfigType
 
 _LOGGER = logging.getLogger(__name__)
 
-PLATFORMS: list[Platform] = [Platform.SENSOR]
 
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up My Custom Device from a config entry."""
-    hass.data.setdefault(DOMAIN, {})
-    
-    # TODO: Initialize your device/API client here
-    # Example: client = MyDeviceClient(entry.data["host"])
-    
-    # Store the client or data for use by platforms
-    hass.data[DOMAIN][entry.entry_id] = {
-        "name": entry.data.get("name", "My Device"),
-    }
-    
-    # Forward the setup to the sensor platform
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the My Custom Device integration."""
+    _LOGGER.info("My Custom Device integration is loading")
     return True
-
-
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a config entry."""
-    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
-        hass.data[DOMAIN].pop(entry.entry_id)
-    
-    return unload_ok
